64a6d8cb6b83e402dae7d0de6bdc96627d5e8c6d
Cihan Yetişken <cyetisken@gmail.com>
2019-12-17 12:12:24 +0300

    Döngü Örnekleri-Diziler Giriş

---
+++ Gazi.KazanMyo.Sube2.DizilerApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gazi.KazanMyo.Sube2.DizilerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Dizi-Array
+
+            //int[] dizi = { 5,8,7,9 };
+
+            int[] dizi = new int[3];
+
+            dizi[0] = 58;
+            dizi[1] = 7;
+            dizi[2] = 20;
+
+            //Console.WriteLine(dizi[0]);
+            //Console.WriteLine(dizi[1]);
+            //Console.WriteLine(dizi[2]);
+            
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                Console.WriteLine(dizi[i]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
--- Gazi.KazanMyo.Sube2.DongulerApp/Program.cs
+++ Gazi.KazanMyo.Sube2.DongulerApp/Program.cs
@@ -36,40 +36,105 @@ namespace Gazi.KazanMyo.Sube2.DongulerApp
             //}
             //Console.WriteLine(sonuc);
 
-            Console.WriteLine("Birinci sayıyı giriniz:");
-            int sayi1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Birinci sayıyı giriniz:");
+            //int sayi1 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("İkinci sayıyı giriniz:");
-            int sayi2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("İkinci sayıyı giriniz:");
+            //int sayi2 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Tek/Çift?");
-            int sonuc = Console.ReadLine().ToLower() == "tek" ? 1 : 0;
+            //Console.WriteLine("Tek/Çift?");
+            //int sonuc = Console.ReadLine().ToLower() == "tek" ? 1 : 0;
 
-            int toplam = 0;
+            //int toplam = 0;
 
             //sayi1=10
             //sayi2=5
 
-            if (sayi1 > sayi2)
-            {
-                int temp = sayi1;
-                sayi1 = sayi2;
-                sayi2 = temp;
-            }
+            //if (sayi1 > sayi2)
+            //{
+            //    int temp = sayi1;
+            //    sayi1 = sayi2;
+            //    sayi2 = temp;
+            //}
             //
             //5%2=1
             //Github-Visual Studio Github Extension
             //Depo-Repository
-            for (int i = sayi1; i <= sayi2; i++)
-            {
-                if (i % 2 == sonuc)//
-                {
-                    Console.WriteLine(i);
-                    toplam = toplam + i;
-                }     
-            }
-            Console.WriteLine(toplam);
-            Console.ReadKey();
+            //for (int i = sayi1; i <= sayi2; i++)
+            //{
+            //    if (i % 2 == sonuc)
+            //    {
+            //        Console.WriteLine(i);
+            //        toplam = toplam + i;
+            //    }     
+            //}
+            //Console.WriteLine(toplam);
+
+            //int sayac = 6;
+            //while (sayac<5)
+            //{               
+            //    Console.WriteLine("Deneyelim");
+            //    sayac++;
+            //}
+
+
+            //do
+            //{
+            //    Console.WriteLine("Deneme");
+            //} while (false);
+
+
+            //int toplam = 0;
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Sayı Giriniz:");
+            //    int sayi = int.Parse(Console.ReadLine());
+            //    toplam += sayi;
+            //    if (sayi==0)
+            //    {
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine($"Sayıların Toplamı:{toplam}");
+
+            //Console.WriteLine("Sayı Giriniz:");
+            //int sayi = int.Parse(Console.ReadLine());
+            //int toplam = 0;
+            //while (sayi!=0)
+            //{
+            //    Console.WriteLine("Sayı giriniz:");
+            //    sayi = int.Parse(Console.ReadLine());
+            //    toplam += sayi;
+            //}
+            //Console.WriteLine($"Sayıların Toplamı:{toplam}");
+
+            //int sayi = 1;
+            //int toplam = 0;
+
+            //while (sayi != 0)
+            //{
+            //    Console.WriteLine("Sayı giriniz:");
+            //    sayi = int.Parse(Console.ReadLine());
+            //    toplam += sayi;
+            //}
+
+            //Console.WriteLine($"Sayıların Toplamı:{toplam}");
+
+            //double toplam = 0;
+            //int sayac=0;
+            //double sayi;
+            //do
+            //{
+            //    Console.WriteLine("Sayı giriniz:");
+            //    sayi = double.Parse(Console.ReadLine());
+            //    toplam += sayi;
+            //    sayac++;
+
+            //} while (sayi != 0);
+
+            //Console.WriteLine($"Girdiğinisa sayıların toplamı:{toplam}\nOrtalaması:{toplam/(sayac-1)}");
+            //Console.ReadKey();
         }
     }
 }

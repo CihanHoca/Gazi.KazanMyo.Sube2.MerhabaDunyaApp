5034c8d29abb4a2ae16dff74834e27415a22501d
Cihan Yetişken <cyetisken@gmail.com>
2019-12-24 12:11:20 +0300

    İç içe döngüler 2 boyutlu diziler

--- Gazi.KazanMyo.Sube2.DizilerApp/Program.cs
+++ Gazi.KazanMyo.Sube2.DizilerApp/Program.cs
@@ -13,20 +13,106 @@ namespace Gazi.KazanMyo.Sube2.DizilerApp
 
             //int[] dizi = { 5,8,7,9 };
 
-            int[] dizi = new int[3];
+            //int[] dizi = new int[3];
 
-            dizi[0] = 58;
-            dizi[1] = 7;
-            dizi[2] = 20;
+
+
+
+            //dizi[0] = 58;
+            //dizi[1] = 7;
+            //dizi[2] = 20;
 
             //Console.WriteLine(dizi[0]);
             //Console.WriteLine(dizi[1]);
             //Console.WriteLine(dizi[2]);
-            
-            for (int i = 0; i < dizi.Length; i++)
+
+
+            //Random rnd = new Random();
+            //int sayi = rnd.Next();
+
+            //for (int i = 0; i < dizi.Length; i++)
+            //{
+            //    Console.WriteLine(dizi[i]);
+            //}
+
+            //int[] dizi = new int[10];
+
+            //Random rnd=new Random();  
+
+            //for (int i = 0; i < dizi.Length; i++)
+            //{
+            //    dizi[i] = rnd.Next(100); 
+            //}
+            //Array.Sort(dizi);
+            //for (int i = 0; i < dizi.Length; i++)
+            //{
+            //   Console.WriteLine(dizi[i]);
+            //}
+
+
+            //Console.WriteLine("Kaç adet isim?");
+            //int adet = int.Parse(Console.ReadLine());
+
+            //string[] isimler = new string[adet];
+
+            //for (int i = 0; i < adet; i++)
+            //{
+            //    Console.WriteLine("İsim Giriniz:");
+            //    isimler[i] = Console.ReadLine();
+            //}
+
+            //Array.Sort(isimler);
+
+            //for (int i = 0; i < isimler.Length; i++)
+            //{
+            //    Console.WriteLine(isimler[i]);
+            //}
+
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 2; j++)
+            //    {
+
+            //    }
+            //}
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //*
+            //**
+            //***
+            //****
+            //*****
+
+
+            int[,] dizi = new int[3, 2];
+            Random rnd = new Random();
+
+            for (int i = 0; i < dizi.GetLength(0); i++)
             {
-                Console.WriteLine(dizi[i]);
+                for (int j = 0; j < dizi.GetLength(1); j++)
+                {
+                    dizi[i, j] = rnd.Next(100);
+                }
             }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(dizi[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+
             Console.ReadKey();
 
         }
